e62362f78229cc87a3ac426ff40273a0a2e5d3f1
mrutyunjay17 <samdarkangels17@gmail.com>
2021-10-24 18:06:33 +0530

    update

--- utility.py
+++ utility.py
@@ -35,10 +35,13 @@ def get_processed_dataset(weather_df = None, folder_path = DEFAULT_PATH_FOLDER_T
     
     '''
         Get pre-processed weather datset according to data analysis.
-        Indexes dataframe with datetime
-        weather_df : The dataframe will get processed for further ETL operations if provided, 
+        Arguments:
+            weather_df  : The dataframe will get processed for further ETL operations if provided, 
                     or else provide csv dataset path to load
-        folder_path , csv_dataset_name : (Optional) if weather_df = None, then this parameter can be used to load the dataset manually 
+            folder_path : (Optional) if weather_df = None, then this parameter can be used to load the dataset manually
+            csv_dataset_name : (Optional) if weather_df = None, then this parameter can be used to load the dataset manually
+        Returns:
+            Pandas Dataframe with indexed with datetime 
     '''
     
     if(weather_df == None):
@@ -77,8 +80,42 @@ def replace_false_cloud_coverage_with_previous(weather_df):
     '''
         Handle Wrongly labelled data
         Replace total_cloud_coverage_pct < -1 with it's previous minute readings.
+        Returns:
+            Pands Dataframe
     '''
     weather_df['total_cloud_coverage_pct'] = weather_df.total_cloud_coverage_pct.mask(weather_df.total_cloud_coverage_pct < -1) \
                                                 .ffill().astype(int)
     return weather_df
 
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	"""
+	Frame a time series as a supervised learning dataset.
+	Arguments:
+		data: Sequence of observations as a list or NumPy array.
+		n_in: Number of lag observations as input (X).
+		n_out: Number of observations as output (y).
+		dropnan: Boolean whether or not to drop rows with NaN values.
+	Returns:
+		Pandas DataFrame of series framed for supervised learning.
+	"""
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = pd.DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = pd.concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
